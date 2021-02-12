a68da581f8f7b6ed0aa784fdde9d17433bb73289
leeshan77 <leeshan77@naver.com>
2021-02-13 07:16:08 +0900

    config 설정변경

--- config/production.py
+++ config/production.py
@@ -1,7 +1,5 @@
-import os
-
-BASE_DIR = os.path.dirname(__file__)
+from config.default import *
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = "dev"
+SECRET_KEY = b"\xa2\x06'\xccP~\x19\xf7\xef\x9c[\xad\xdb\x1a\xb97"
